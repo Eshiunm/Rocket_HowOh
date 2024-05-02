75a8eb6c6fc379adeed3d4d063a8027a850fe653
Avain <sports113499@gmail.com>
2024-05-02 16:11:22 +0800

    feat: add Landlord-Management Add-New page and add Add-New page procedure part data

---
+++ src/Components/AddNewProcedure.tsx
@@ -0,0 +1,42 @@
+export default function AddNewProcedure() {
+  const procedure = [
+    {
+      title: '基本資訊',
+      isActive: true,
+    },
+    {
+      title: '房源照片',
+      isActive: false,
+    },
+    {
+      title: '特色',
+      isActive: false,
+    },
+    {
+      title: '設備',
+      isActive: false,
+    },
+    {
+      title: '雜支',
+      isActive: false,
+    },
+    {
+      title: '訂金與租金',
+      isActive: false,
+    },
+    {
+      title: '房源介紹',
+      isActive: false,
+    },
+    {
+      title: '租客限制',
+      isActive: false,
+    }
+  ];
+
+  return (
+  <>
+
+  </>
+  );
+}
