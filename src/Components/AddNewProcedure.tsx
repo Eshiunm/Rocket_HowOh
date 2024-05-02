b068ef9a7b185bf2e64779ea1574c5bc9526d71c
Avain <sports113499@gmail.com>
2024-05-02 17:43:27 +0800

    feat: add Add-New pages Procedure steps styles

--- src/Components/AddNewProcedure.tsx
+++ src/Components/AddNewProcedure.tsx
@@ -1,42 +1,60 @@
 export default function AddNewProcedure() {
   const procedure = [
     {
-      title: '基本資訊',
+      title: "基本資訊",
+      isActive: false,
+      isDone: true,
+    },
+    {
+      title: "房源照片",
+      isActive: false,
+      isDone: true,
+    },
+    {
+      title: "特色",
       isActive: true,
+      isDone: false,
     },
     {
-      title: '房源照片',
+      title: "設備",
       isActive: false,
+      isDone: false,
     },
     {
-      title: '特色',
+      title: "雜支",
       isActive: false,
+      isDone: false,
     },
     {
-      title: '設備',
+      title: "訂金與租金",
       isActive: false,
+      isDone: false,
     },
     {
-      title: '雜支',
+      title: "房源介紹",
       isActive: false,
+      isDone: false,
     },
     {
-      title: '訂金與租金',
+      title: "租客限制",
       isActive: false,
-    },
-    {
-      title: '房源介紹',
-      isActive: false,
-    },
-    {
-      title: '租客限制',
-      isActive: false,
+      isDone: false,
     }
   ];
 
   return (
-  <>
-
-  </>
+  <div className="after:content-[''] after:w-[1px] after:h-[480px] after:bg-gray-400 after:absolute after:left-4 after:-z-10 after:top-0">
+    {
+      procedure.map((item, index) => {
+        const {title,isActive,isDone} = item;
+        return (
+          <div key={title} className="flex gap-3 mb-8">
+            <div className={isActive ? "add-new-step-index-active" : "add-new-step-index"}>{isDone?"✓":index + 1}</div>
+            <div className={isActive ? "black leading-8" : "text-gray-400 leading-8"}>{title}</div>
+          </div>
+        )
+      })
+    }
+  </div>
   );
 }
--- src/Pages/LandlordManagementAddNew.tsx
+++ src/Pages/LandlordManagementAddNew.tsx
@@ -1,7 +1,10 @@
+import AddNewProcedure from "../Components/AddNewProcedure";
+{/* <Route path="/landlord-management-add-new" element={<LandlordManagementAddNew />}></Route> */}
+
 export default function LandlordManagementAddNew() {
   return (
-  <>
-
-  </>
+    <div>
+      <AddNewProcedure />
+    </div>
   );
 }
