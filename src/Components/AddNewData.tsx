aafefde95ab68438b74778bc3e50304210a71a2d
Avain <sports113499@gmail.com>
2024-05-03 13:23:06 +0800

    feat: add Add-New address details part

--- src/Components/AddNewData.tsx
+++ src/Components/AddNewData.tsx
@@ -32,7 +32,6 @@ export default function AddNewData() {
             </div>
             <div className="add-new-input-block">
               <label htmlFor="houseDistrict" className="text-sm">區域</label>
-              {/* <input type="text" id="houseDistrict" className="add-new-input" placeholder="新興區" {...register("houseDistrict", { required: true })} /> */}
               <select id="houseDistrict" className="add-new-input" {...register("houseDistrict", { required: true })}>
                 {
                   cities.filter(({city}) => city === "高雄市")[0].districts.map((district) => <option value={district} key={district} >{district}</option>)
@@ -44,20 +43,28 @@ export default function AddNewData() {
               <input type="text" id="houseStreet" className="add-new-input" placeholder="中正三路" {...register("houseStreet", { required: true })} />
             </div>
           </div>
-          {/* <div className="flex gap-6">
-            <div className="add-new-input-block">
+          <div className="flex gap-6">
+            <div className="add-new-input-block after:content-['巷'] add-new-input-unit">
               <label htmlFor="houseLane" className="text-sm">巷</label>
-              <input type="text" id="houseLane" className="add-new-input" placeholder="選填" {...register("houseLane", { required: true })} />
+              <input type="text" id="houseLane" className="add-new-input" placeholder="選填" {...register("houseLane")} />
             </div>
-            <div className="add-new-input-block">
+            <div className="add-new-input-block after:content-['弄'] add-new-input-unit">
               <label htmlFor="houseAlley" className="text-sm">弄</label>
-              <input type="text" id="houseAlley" className="add-new-input" placeholder="選填" {...register("houseAlley", { required: true })} />
+              <input type="text" id="houseAlley" className="add-new-input" placeholder="選填" {...register("houseAlley")} />
+            </div>
+            <div className="add-new-input-block after:content-['號'] add-new-input-unit">
+              <label htmlFor="houseNumber" className="text-sm">號</label>
+              <input type="text" id="houseNumber" className="add-new-input" placeholder="23" {...register("houseNumber")} />
             </div>
             <div className="add-new-input-block">
-              <label htmlFor="houseNumber" className="text-sm">號</label>
-              <input type="text" id="houseNumber" className="add-new-input" placeholder="23" {...register("houseNumber", { required: true })} />
+              <label htmlFor="houseFloor" className="text-sm">樓層</label>
+              <input type="text" id="houseFloor" className="add-new-input" placeholder="23" {...register("houseFloor")} />
             </div>
-          </div> */}
+            <div className="add-new-input-block">
+              <label htmlFor="houseTotalFloors" className="text-sm">總樓數</label>
+              <input type="text" id="houseTotalFloors" className="add-new-input" placeholder="23" {...register("houseTotalFloors")} />
+            </div>
+          </div>
           <button type="submit" className="self-start px-4 py-2 bg-slate-700 rounded-3xl text-white">下一步</button>
         </form>
         {
