90c21b3d96d935a39d7aa83a186aff215d3b494d
Avain <sports113499@gmail.com>
2024-05-03 21:56:11 +0800

    feat: add Add-New Page procedure click to change styles

--- src/Components/AddNewProcedure.tsx
+++ src/Components/AddNewProcedure.tsx
@@ -1,55 +1,29 @@
-export default function AddNewProcedure() {
-  const procedure = [
-    {
-      title: "基本資訊",
-      isActive: false,
-      isDone: true,
-    },
-    {
-      title: "房源照片",
-      isActive: false,
-      isDone: true,
-    },
-    {
-      title: "特色",
-      isActive: true,
-      isDone: false,
-    },
-    {
-      title: "設備",
-      isActive: false,
-      isDone: false,
-    },
-    {
-      title: "雜支",
-      isActive: false,
-      isDone: false,
-    },
-    {
-      title: "訂金與租金",
-      isActive: false,
-      isDone: false,
-    },
-    {
-      title: "房源介紹",
-      isActive: false,
-      isDone: false,
-    },
-    {
-      title: "租客限制",
-      isActive: false,
-      isDone: false,
-    }
-  ];
+export default function AddNewProcedure({procedure, setProcedure}) {
+  const handleProcedureClick = (title: string) => {
+    const newProcedure = procedure.map((item) => {
+      if (item.title === title) {
+        return {
+          ...item,
+          isActive: true,
+        };
+      } else {
+        return {
+          ...item,
+          isActive: false,
+        };
+      }
+    });
+    setProcedure(newProcedure);
+  }
 
   return (
     <div className="p-10 border-r border-gray-400">
       <div className="relative after:content-[''] after:w-[1px] after:h-full after:bg-gray-400 after:absolute after:left-4 after:-z-10 after:top-0">
         {
-          procedure.map((item, index) => {
+          procedure.map((item, index: number) => {
             const {title,isActive,isDone} = item;
             return (
-              <button key={title} className="flex gap-3 mb-8">
+              <button key={title} className="flex gap-3 mb-8" onClick={()=> handleProcedureClick(title)}>
                 <div className={isActive ? "add-new-step-index-active" : "add-new-step-index"}>{isDone?"✓":index + 1}</div>
                 <div className={isActive ? "black leading-8" : "text-gray-400 leading-8"}>{title}</div>
               </button>
--- src/Pages/LandlordManagementAddNew.tsx
+++ src/Pages/LandlordManagementAddNew.tsx
@@ -1,12 +1,59 @@
+import { useState } from "react";
 import AddNewData from "../Components/AddNewData";
 import AddNewProcedure from "../Components/AddNewProcedure";
 {/* <Route path="/landlord-management-add-new" element={<LandlordManagementAddNew />}></Route> */}
 
 export default function LandlordManagementAddNew() {
+  const procedureList = [
+    {
+      title: "基本資訊",
+      isActive: true,
+      isDone: false,
+    },
+    {
+      title: "房源照片",
+      isActive: false,
+      isDone: false,
+    },
+    {
+      title: "特色",
+      isActive: false,
+      isDone: false,
+    },
+    {
+      title: "設備",
+      isActive: false,
+      isDone: false,
+    },
+    {
+      title: "雜支",
+      isActive: false,
+      isDone: false,
+    },
+    {
+      title: "訂金與租金",
+      isActive: false,
+      isDone: false,
+    },
+    {
+      title: "房源介紹",
+      isActive: false,
+      isDone: false,
+    },
+    {
+      title: "租客限制",
+      isActive: false,
+      isDone: false,
+    }
+  ];
+  const [procedure, setProcedure] = useState(procedureList);
+
   return (
-    <div className="flex">
-      <AddNewProcedure />
-      <AddNewData />
+    <div className="container max-w-5xl mx-auto">
+      <div className="flex">
+        <AddNewProcedure procedure={procedure} setProcedure={setProcedure} />
+        <AddNewData />
+      </div>
     </div>
   );
 }
