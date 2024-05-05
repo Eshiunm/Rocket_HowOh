dcfdda343886b47cd39c7d426d81fe2ebb604aa9
Eshiunm <eshiunm@gmail.com>
2024-05-05 23:20:20 +0800

    feat: add container & screens in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    container: {
+      center: true,
+      padding: "12px",
+    },
+    screens: {
+      sm: "540px",
+      md: "720px",
+      lg: "960px",
+      xl: "1140px",
+      "2xl": "1400px",
+      "3xl": "1608px",
+    },
     extend: {},
   },
   plugins: [],
