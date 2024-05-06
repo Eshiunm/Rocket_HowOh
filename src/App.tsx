e8603b00bc313c45a46ec4275b85db12260311eb
Avain <sports113499@gmail.com>
2024-05-06 15:00:11 +0800

    feat: add post route

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,7 @@ function App() {
           <Route path="/" element={<h1>這是首頁</h1>}></Route>
           <Route path="/login" element={<h1>你在登入頁</h1>}></Route>
           <Route path="/signup" element={<h1>你在註冊頁</h1>}></Route>
+          <Route path="/post" element={<h1>你在刊登房源頁面</h1>}></Route>
         </Routes>
       </BrowserRouter>
     </div>
