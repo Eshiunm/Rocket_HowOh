399dd5d7321d75a622729c8f7372484009683776
Avain <sports113499@gmail.com>
2024-05-06 17:38:25 +0800

    feat: add restriction of sex

--- src/Components/Posts/Restrictions.tsx
+++ src/Components/Posts/Restrictions.tsx
@@ -1,7 +1,42 @@
+import { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { ProcedureContext } from "../../Pages/LandlordManagementAddNew";
+import RadioSelect from "../RadioSelect";
+
 export default function Restrictions() {
+  const { register, handleSubmit, formState: {errors} } = useForm();
+  const { handleProcedureClick, handleProcedureDone } = useContext(ProcedureContext);
+  const onSubmit = (data) => {
+    console.log(data);
+    handleProcedureDone(7);
+    alert("成功刊登房源");
+  };
+
   return (
-    <div>
-      <h3>租客限制</h3>
-    </div>
+    <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
+      <button type="button" className="self-start add-new-back-btn"  onClick={() => handleProcedureClick("房源介紹")}>上一步</button>
+      <h3 className="add-new-title">租客限制</h3>
+      <p>您若設定租客限制，不符合條件的用戶將不會得到您的聯絡資訊。</p>
+      <div>
+        <h4 className="add-new-small-title">是否設定條件</h4>
+        <fieldset className="flex gap-6" >
+          <RadioSelect option={{id: "yes", title: "是", type: "isRestricted"}} register={register} />
+          <RadioSelect option={{id: "no", title: "否，開放所有租客", type: "isRestricted"}} register={register} />
+        </fieldset>
+      </div>
+      <div>
+        <h4 className="add-new-small-title">性別</h4>
+        <fieldset className="flex gap-6" >
+          <RadioSelect option={{id: "male", title: "男", type: "sexRestriction"}} register={register} />
+          <RadioSelect option={{id: "female", title: "女", type: "sexRestriction"}} register={register} />
+          <RadioSelect option={{id: "noLimit", title: "性別友善", type: "sexRestriction"}} register={register} />
+        </fieldset>
+      </div>
+      <div>
+        <h4 className="add-new-small-title">排除職業</h4>
+          <h5 className="mb-2 text-sm">職業1</h5>
+      </div>
+      <button type="submit" className="add-new-btn add-new-next-btn">下一步</button>
+    </form>
   );
 }
