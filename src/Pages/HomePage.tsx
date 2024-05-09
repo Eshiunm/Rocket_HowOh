6ff5c5b2d6dfe984357e23201f27b7725cd09e1a
Eshiunm <eshiunm@gmail.com>
2024-05-09 13:05:51 +0800

    style: modify search style

--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -64,7 +64,7 @@ function HomePage() {
                   />
                   <label
                     htmlFor="floating_outlined"
-                    className="absolute text-sans-body1 text-black duration-300 transform -translate-y-4  top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:border-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-[3px] peer-focus:scale-75 peer-focus:-translate-y-4 start-3"
+                    className="absolute text-sans-body1 text-black duration-200 transform -translate-y-4 scale-75 top-[3px] z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:text-Brand-30 peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-[3px] peer-focus:scale-75 peer-focus:-translate-y-4 start-3"
                   >
                     搜尋
                   </label>
