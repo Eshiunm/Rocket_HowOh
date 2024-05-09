86daea0dd3ac52b8c65f19a91ad5cac97ad22adc
Eshiunm <eshiunm@gmail.com>
2024-05-09 12:44:47 +0800

    feat: add dropdown & search

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -7,14 +7,10 @@ function Header() {
   const isLogin = false;
   return (
     <>
-      <nav className="bg-white sticky top-0">
-        <div className="container flex justify-between items-center py-2 bg-white z-20">
+      <nav className="bg-white sticky top-0 z-50">
+        <div className="container flex justify-between items-center py-2 bg-white">
           <Link to="/" className="flex items-center">
-            <img
-              src={logo}
-              alt="howoh logo"
-              className="mr-3"
-            />
+            <img src={logo} alt="howoh logo" className="mr-3" />
             <h1 className="logo font-Dela-Gothic-One text-dela-display4">
               好窩
             </h1>
--- src/Pages/HomePage.tsx
+++ src/Pages/HomePage.tsx
@@ -1,14 +1,18 @@
 import { useForm } from "react-hook-form";
+import dropdownIcon from "../assets/imgs/icons/dropdownIcon.svg";
+import searchIcon from "../assets/imgs/icons/searchIcon.svg";
+import { useState } from "react";
 
 function HomePage() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
+  const [isInputFocused, setIsInputFocused] = useState(false);
+  const { handleSubmit } = useForm();
+
+  const onSubmit = data => {
+    console.log(data);
+  };
   return (
     <>
-      <section className="section-search bg-homeSearchImg bg-center bg-cover  h-[842px]">
+      <section className="section-search bg-homeSearchImg bg-center bg-cover h-[842px]">
         {/* Title */}
         <div className="container pt-20 mb-8">
           <h2 className="w-[559px] mx-auto text-center bg-Neutral-10 text-white font-Dela-Gothic-One text-dela-display1 pl-8   rounded-[12px]">
@@ -20,7 +24,56 @@ function HomePage() {
         </div>
         {/* Filter form */}
         <div className="container layout-grid">
-          <div className="col-start-2 col-span-10"></div>
+          <div className="col-start-2 col-span-10">
+            <form
+              onSubmit={handleSubmit(onSubmit)}
+              className="bg-white rounded-[20px] p-8"
+            >
+              <div className="flex gap-6">
+                {/* dropdown component */}
+                <div className="relative w-60">
+                  <span className="absolute text-sans-caption text-black top-[-10px] z-1 bg-white px-[2px] start-3">
+                    縣市
+                  </span>
+                  <div
+                    tabIndex={0} // 新增這個屬性可獲得 focus 焦點
+                    className="flex justify-between h-12 p-3 bg-transparent rounded-[4px] border-1 border border-black appearance-none focus:outline-none focus:ring-0 focus:border-Brand-30"
+                  >
+                    <span className="text-black">高雄市</span>
+                    <img
+                      src={dropdownIcon}
+                      className="scale-[.8]"
+                      alt="dropdownIcon"
+                    />
+                  </div>
+                </div>
+                {/* search component */}
+                <div
+                  tabIndex={0}
+                  className={`relative flex w-full border p-3  rounded-[4px] ${
+                    isInputFocused ? "border-Brand-30 " : "border-black"
+                  }`}
+                >
+                  <input
+                    type="text"
+                    id="floating_outlined"
+                    className="block w-full p-0 pl-1 text-black bg-transparent border-none appearance-none focus:ring-0 peer"
+                    placeholder=""
+                    onFocus={() => setIsInputFocused(true)}
+                    onBlur={() => setIsInputFocused(false)}
+                  />
+                  <label
+                    htmlFor="floating_outlined"
+                    className="absolute text-sans-body1 text-black duration-300 transform -translate-y-4  top-2 z-10 origin-[0] bg-white px-2 peer-focus:px-2 peer-focus:border-black peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-[3px] peer-focus:scale-75 peer-focus:-translate-y-4 start-3"
+                  >
+                    搜尋
+                  </label>
+                  <img src={searchIcon} alt="searchIcon" />
+                </div>
+              </div>
+              <input type="submit" value="搜尋" />
+            </form>
+          </div>
         </div>
       </section>
     </>
