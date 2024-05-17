8531f5e34510977c06437b5bf53637d080d8f699
Avain <sports113499@gmail.com>
2024-05-17 15:46:39 +0800

    feat: add main feature, near by facilities, house features, equipment sections validations

--- src/Components/landLordManagement/posts/procedures/Facilities.tsx
+++ src/Components/landLordManagement/posts/procedures/Facilities.tsx
@@ -1,7 +1,6 @@
 import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { ProcedureContext } from "../../../../pages/landlordManagement/AddNew";
-import CheckBox from "../CheckBox";
 import {
   houseFeatures,
   mainFeatures,
@@ -9,7 +8,6 @@ import {
   transportations,
   equipments
 } from "../../../../constants/featureList";
-import CheckToShowInput from "../CheckToShowInput";
 
 export default function Facilities() {
   const [selectedMainFeatures, setSelectedMainFeatures] = useState({
@@ -26,12 +24,17 @@ export default function Facilities() {
     isNearTrainStation: false,
     isNearHSR: false,
   });
-  const { handleSubmit } = useForm();
+  const { register, handleSubmit } = useForm();
   const { handleProcedureClick, handleProcedureDone } =
     useContext(ProcedureContext);
-  const onSubmit = () => {
-    handleProcedureDone(2);
-    handleProcedureClick("費用");
+  const onSubmit = (data) => {
+    const formData = {
+      ...selectedMainFeatures,
+      ...data,
+    }
+    console.log(formData)
+    // handleProcedureDone(2);
+    // handleProcedureClick("費用");
   };
 
   const handleButtonClick = (id) => {
@@ -86,6 +89,7 @@ export default function Facilities() {
                     type="checkbox"
                     id={id}
                     className="w-5 h-5 text-black focus:ring-transparent rounded-sm border-2 border-black cursor-pointer"
+                    {...register(id)}
                   />
                   {title}
                 </label>
@@ -103,6 +107,7 @@ export default function Facilities() {
                     type="checkbox"
                     id={id}
                     className="w-5 h-5 text-black focus:ring-transparent rounded-sm border-2 border-black cursor-pointer"
+                    {...register(id)}
                   />
                   {title}
                 </label>
@@ -120,6 +125,7 @@ export default function Facilities() {
                     type="checkbox"
                     id={id}
                     className="w-5 h-5 text-black focus:ring-transparent rounded-sm border-2 border-black cursor-pointer"
+                    {...register(id)}
                   />
                   {title}
                 </label>
@@ -168,61 +174,5 @@ export default function Facilities() {
         </div>
       </form>
     </div>
-    // <form className="flex flex-col gap-6" onSubmit={handleSubmit(onSubmit)}>
-    //   <button
-    //     type="button"
-    //     className="self-start add-new-back-btn"
-    //     onClick={() => handleProcedureClick("照片")}
-    //   >
-    //     上一步
-    //   </button>
-    //   <h3 className="add-new-title">重點特色</h3>
-    //   <div>
-    //     {mainFeatures.map(({ id, title }) => (
-    //       <CheckBox id={id} title={title} register={register} key={id} />
-    //     ))}
-    //   </div>
-    //   <h3 className="add-new-title">其他特色</h3>
-    //   <div>
-    //     <div>
-    //       <h4 className="add-new-small-title">附近機能</h4>
-    //       <div>
-    //         {nearByFacilities.map(({ id, title }) => (
-    //           <CheckBox id={id} title={title} register={register} key={id} />
-    //         ))}
-    //       </div>
-    //     </div>
-    //     <div>
-    //       <h4 className="add-new-small-title">屋源特色</h4>
-    //       <div>
-    //         {houseFeatures.map(({ id, title }) => (
-    //           <CheckBox id={id} title={title} register={register} key={id} />
-    //         ))}
-    //       </div>
-    //     </div>
-    //     <div>
-    //       <h4 className="add-new-small-title">交通</h4>
-    //       <div className="flex gap-6">
-    //         {transportations.map(({ id, title }) => (
-    //           <CheckToShowInput
-    //             id={id}
-    //             title={title}
-    //             register={register}
-    //             key={id}
-    //           />
-    //         ))}
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <h3 className="add-new-title">設備</h3>
-    //   <div>
-    //     {equipments.map(({ id, title }) => (
-    //       <CheckBox id={id} title={title} register={register} key={id} />
-    //     ))}
-    //   </div>
-    //   <button type="submit" className="add-new-btn add-new-next-btn">
-    //     下一步
-    //   </button>
-    // </form>
   );
 }
