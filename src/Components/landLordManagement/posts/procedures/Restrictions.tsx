5dec5061990f7d9e28c78a82f9b38e213f6b0244
Avain <sports113499@gmail.com>
2024-05-18 23:32:16 +0800

    feat: add hasTenantRestrictions part watching and show choose or not

--- src/Components/landLordManagement/posts/procedures/Restrictions.tsx
+++ src/Components/landLordManagement/posts/procedures/Restrictions.tsx
@@ -4,11 +4,12 @@ import { ProcedureContext } from "../../../../pages/landlordManagement/AddNew";
 import { occupations } from "../../../../constants/occupations";
 
 export default function Restrictions() {
-  const {
-    register,
-    handleSubmit,
-    // formState: { errors },
-  } = useForm();
+  const { register, handleSubmit, formState: { errors }, watch } = useForm({
+    defaultValues: {
+      hasTenantRestrictions: "false",
+    },
+  });
+  const hasTenantRestrictions = watch("hasTenantRestrictions");
   const { handleProcedureClick, handleProcedureDone } =
     useContext(ProcedureContext);
   const onSubmit = () => {
@@ -27,111 +28,117 @@ export default function Restrictions() {
             <label htmlFor="hasRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
               <input
                 type="radio"
-                name="hasTenantRestrictions"
                 id="hasRestriction"
+                value={"true"}
                 className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                {...register("hasTenantRestrictions", { required: true })}
               />
               是
             </label>
             <label htmlFor="noRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
               <input
                 type="radio"
-                name="hasTenantRestrictions"
                 id="noRestriction"
+                value={"false"}
                 className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                {...register("hasTenantRestrictions", { required: true })}
               />
               否
             </label>
           </fieldset>
         </div>
-        <div className="flex flex-col gap-3 mb-10">
-          <h5 className="text-sans-b-body1 text-Landlord-40">性別</h5>
-          <fieldset className="layout-grid">
-            <label htmlFor="maleRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
-              <input
-                type="radio"
-                name="genderRestriction"
-                id="maleRestriction"
-                className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
-              />
-              男
-            </label>
-            <label htmlFor="femaleRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
-              <input
-                type="radio"
-                name="genderRestriction"
-                id="femaleRestriction"
-                className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
-              />
-              女
-            </label>
-            <label htmlFor="noSexRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
-              <input
-                type="radio"
-                name="genderRestriction"
-                id="noSexRestriction"
-                className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
-              />
-              不限
-            </label>
-          </fieldset>
-          <h5 className="text-sans-b-body1 text-Landlord-40">職業</h5>
-          <fieldset className="layout-grid">
-            <label htmlFor="hasJobRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
-              <input
-                type="radio"
-                name="jobRestriction"
-                id="hasJobRestriction"
-                className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
-              />
-              限制
-            </label>
-            <label htmlFor="noJobRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
-              <input
-                type="radio"
-                name="jobRestriction"
-                id="noJobRestriction"
-                className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
-              />
-              不限
-            </label>
-          </fieldset>
-          <div className="layout-grid">
-            <div className="col-span-6 mt-2.5">
-              <div
-                tabIndex={0}
-                className="relative flex w-full rounded border-black border"
-              >
-                <select
-                  id="city"
-                  className="block w-full p-3 pl-5 text-sans-body1 text-black bg-transparent border-none appearance-none focus:ring-0"
-                  {...register("jobRestriction", {
-                    required: { value: true, message: "必填欄位" },
-                  })}
-                >
-                  {occupations.map(({ id, title: occupation }) => (
-                      <option value={occupation} key={id}>
-                        {occupation}
-                      </option>
-                    )
-                  )}
-                </select>
-                <label
-                  htmlFor="city"
-                  className="absolute text-sans-body1 text-Neutral-50 duration-200 transform -translate-y-4 scale-75 top-[3px] z-10 origin-[0] bg-white px-2  start-3"
-                >
-                  職業類別
+        {
+          hasTenantRestrictions === "true" && (
+            <div className="flex flex-col gap-3 mb-10">
+              <h5 className="text-sans-b-body1 text-Landlord-40">性別</h5>
+              <fieldset className="layout-grid">
+                <label htmlFor="maleRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
+                  <input
+                    type="radio"
+                    name="genderRestriction"
+                    id="maleRestriction"
+                    className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                  />
+                  男
                 </label>
+                <label htmlFor="femaleRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
+                  <input
+                    type="radio"
+                    name="genderRestriction"
+                    id="femaleRestriction"
+                    className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                  />
+                  女
+                </label>
+                <label htmlFor="noSexRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
+                  <input
+                    type="radio"
+                    name="genderRestriction"
+                    id="noSexRestriction"
+                    className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                  />
+                  不限
+                </label>
+              </fieldset>
+              <h5 className="text-sans-b-body1 text-Landlord-40">職業</h5>
+              <fieldset className="layout-grid">
+                <label htmlFor="hasJobRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
+                  <input
+                    type="radio"
+                    name="jobRestriction"
+                    id="hasJobRestriction"
+                    className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                  />
+                  限制
+                </label>
+                <label htmlFor="noJobRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
+                  <input
+                    type="radio"
+                    name="jobRestriction"
+                    id="noJobRestriction"
+                    className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                  />
+                  不限
+                </label>
+              </fieldset>
+              <div className="layout-grid">
+                <div className="col-span-6 mt-2.5">
+                  <div
+                    tabIndex={0}
+                    className="relative flex w-full rounded border-black border"
+                  >
+                    <select
+                      id="city"
+                      className="block w-full p-3 pl-5 text-sans-body1 text-black bg-transparent border-none appearance-none focus:ring-0"
+                      {...register("jobRestriction", {
+                        required: { value: true, message: "必填欄位" },
+                      })}
+                    >
+                      {occupations.map(({ id, title: occupation }) => (
+                          <option value={occupation} key={id}>
+                            {occupation}
+                          </option>
+                        )
+                      )}
+                    </select>
+                    <label
+                      htmlFor="city"
+                      className="absolute text-sans-body1 text-Neutral-50 duration-200 transform -translate-y-4 scale-75 top-[3px] z-10 origin-[0] bg-white px-2  start-3"
+                    >
+                      職業類別
+                    </label>
+                  </div>
+                </div>
+                <div className="col-span-12">
+                  <button className="letter-button-light">
+                    <span>新增職業</span>
+                    <span className="material-symbols-outlined">add</span>
+                  </button>
+                </div>
               </div>
             </div>
-            <div className="col-span-12">
-              <button className="letter-button-light">
-                <span>新增職業</span>
-                <span className="material-symbols-outlined">add</span>
-              </button>
-            </div>
-          </div>
-        </div>
+          )
+        }
         <div className="pt-10 flex justify-between">
           <button 
             type="button"
