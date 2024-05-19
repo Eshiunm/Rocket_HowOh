3bbd33ba18e80e1266d683511bcbe2bfb3c91057
Avain <sports113499@gmail.com>
2024-05-20 00:27:03 +0800

    feat: add basic information section

--- src/Components/landLordManagement/posts/procedures/Confirm.tsx
+++ src/Components/landLordManagement/posts/procedures/Confirm.tsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 import { RootState } from "../../../../../redux/store";
 
 export default function Confirm() {
-  const basicInformationContent = useSelector((state: RootState) => state.basicInformationContent);
+  const { formData } = useSelector((state: RootState) => state.basicInformationContent);
   const photosUpload = useSelector((state: RootState) => state.photosUpload);
   const facilitiesContent = useSelector((state: RootState) => state.facilitiesContent);
   const expensesContent = useSelector((state: RootState) => state.expensesContent);
@@ -16,7 +16,7 @@ export default function Confirm() {
   const onSubmit = () => {
     // handleProcedureDone(5);
     // handleProcedureClick("限制");
-    console.log(basicInformationContent);
+    console.log(formData);
     console.log(photosUpload);
     console.log(facilitiesContent);
     console.log(expensesContent);
@@ -26,20 +26,116 @@ export default function Confirm() {
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-        <div className="col-span-12 pt-10 flex justify-between">
-          <button 
-            type="button"
-            onClick={() => handleProcedureClick("費用")}
-            className="outline-button-m pr-3 flex items-center"
-          >
-            <span className="material-symbols-outlined">chevron_left</span>
-            <span>上一步</span>
-          </button>
-          <button type="submit" className="filled-button-m pl-3 flex items-center">
-            <span>下一步</span>
-            <span className="material-symbols-outlined">chevron_right</span>
-          </button>
+      <section className="bg-Landlord-99 p-5 rounded-lg">
+        <h3 className="add-new-title">基本資訊</h3>
+        <div className="layout-grid">
+          <div className="col-span-12">
+            <h5 className="add-new-small-title">房源名稱</h5>
+            <p className="confirm-data">{formData.name || "\u00A0"}</p>
+          </div>
+          <div className="col-span-4">
+            <h5 className="add-new-small-title">縣市</h5>
+            <p className="confirm-data">{formData.city || "\u00A0"}</p>
+          </div>
+          <div className="col-span-4">
+            <h5 className="add-new-small-title">區域</h5>
+            <p className="confirm-data">{formData.district || "\u00A0"}</p>
+          </div>
+          <div className="col-span-4">
+            <h5 className="add-new-small-title">路街</h5>
+            <p className="confirm-data">{formData.road || "\u00A0"}</p>
+          </div>
+          <div className="col-span-12 mt-2.5 flex gap-6">
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.lane || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                巷
+              </h5>
+            </div>
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.alley || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                弄
+              </h5>
+            </div>
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.number || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                號
+              </h5>
+            </div>
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.floor || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                樓層
+              </h5>
+            </div>
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.floorTotal || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                總樓數
+              </h5>
+            </div>
+          </div>
+          <div className="col-span-6">
+            <h5 className="add-new-small-title">類型</h5>
+            <p className="confirm-data">{formData.type || "\u00A0"}</p>
+          </div>
+          <div className="col-span-6 flex items-center gap-2 mt-[22px]">
+            <p className="confirm-data flex-1">{formData.ping || "\u00A0"}</p>
+            <h5 className="text-sans-body1 shrink-0">
+              可使用坪數
+            </h5>
+          </div>
+          <h4 className="text-sans-body1 col-span-12 -mb-6">格局</h4>
+          <div className="col-span-12 mt-2.5 flex gap-6">
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.roomNumbers || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                房
+              </h5>
+            </div>
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.livingRoomNumbers || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                廳
+              </h5>
+            </div>
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.bathRoomNumbers || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                衛浴
+              </h5>
+            </div>
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.balconyNumbers || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                陽台
+              </h5>
+            </div>
+            <div className="flex-1 flex items-center gap-2">
+              <p className="confirm-data flex-1">{formData.parkingSpaceNumbers || "\u00A0"}</p>
+              <h5 className="text-sans-body1 shrink-0">
+                車位
+              </h5>
+            </div>
+          </div>
         </div>
+      </section>
+      <div className="col-span-12 pt-10 flex justify-between">
+        <button 
+          type="button"
+          onClick={() => handleProcedureClick("費用")}
+          className="outline-button-m pr-3 flex items-center"
+        >
+          <span className="material-symbols-outlined">chevron_left</span>
+          <span>上一步</span>
+        </button>
+        <button type="submit" className="filled-button-m pl-3 flex items-center">
+          <span>下一步</span>
+          <span className="material-symbols-outlined">chevron_right</span>
+        </button>
+      </div>
     </form>
   );
 }
