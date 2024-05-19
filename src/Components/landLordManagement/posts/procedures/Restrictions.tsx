b73fe581462a6b1d0eeabc7b4ff8b6bfc5699f4d
Avain <sports113499@gmail.com>
2024-05-19 15:35:45 +0800

    feat: add gender and job restriction sections , add job select function

--- src/Components/landLordManagement/posts/procedures/Restrictions.tsx
+++ src/Components/landLordManagement/posts/procedures/Restrictions.tsx
@@ -1,21 +1,34 @@
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { ProcedureContext } from "../../../../pages/landlordManagement/AddNew";
 import { occupations } from "../../../../constants/occupations";
 
 export default function Restrictions() {
+  const [jobRestrictionAmount,setJobRestrictionAmount] = useState(0)
   const { register, handleSubmit, formState: { errors }, watch } = useForm({
     defaultValues: {
       hasTenantRestrictions: "false",
+      genderRestriction: "性別友善",
+      job: "noJobRestriction"
     },
   });
   const hasTenantRestrictions = watch("hasTenantRestrictions");
+  const job = watch("job");
   const { handleProcedureClick, handleProcedureDone } =
     useContext(ProcedureContext);
   const onSubmit = () => {
     handleProcedureDone(5);
     handleProcedureClick("完成");
   };
+
+  useEffect(() => {
+    if (job === "hasJobRestriction") {
+      setJobRestrictionAmount(1);
+    } else {
+      setJobRestrictionAmount(0);
+    }
+    console.log("執行useEffect");
+  }, [job]);
 
   return (
     <div className="p-5">
@@ -55,27 +68,30 @@ export default function Restrictions() {
                 <label htmlFor="maleRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
                   <input
                     type="radio"
-                    name="genderRestriction"
                     id="maleRestriction"
+                    value="僅限男性"
                     className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                    {...register("genderRestriction", { required: true })}
                   />
                   男
                 </label>
                 <label htmlFor="femaleRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
                   <input
                     type="radio"
-                    name="genderRestriction"
                     id="femaleRestriction"
+                    value="僅限女性"
                     className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                    {...register("genderRestriction", { required: true })}
                   />
                   女
                 </label>
-                <label htmlFor="noSexRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
+                <label htmlFor="noGenderRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
                   <input
                     type="radio"
-                    name="genderRestriction"
-                    id="noSexRestriction"
+                    id="noGenderRestriction"
+                    value="性別友善"
                     className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                    {...register("genderRestriction", { required: true })}
                   />
                   不限
                 </label>
@@ -85,57 +101,70 @@ export default function Restrictions() {
                 <label htmlFor="hasJobRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
                   <input
                     type="radio"
-                    name="jobRestriction"
                     id="hasJobRestriction"
+                    value="hasJobRestriction"
                     className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                    {...register("job", { required: true })}
                   />
                   限制
                 </label>
                 <label htmlFor="noJobRestriction" className="col-span-3 flex items-center gap-2 text-sans-body1">
                   <input
                     type="radio"
-                    name="jobRestriction"
                     id="noJobRestriction"
+                    value="noJobRestriction"
                     className="w-6 h-6 text-black bg-transparent border-black focus:ring-0 focus:ring-transparent"
+                    {...register("job", { required: true })}
                   />
                   不限
                 </label>
               </fieldset>
-              <div className="layout-grid">
-                <div className="col-span-6 mt-2.5">
-                  <div
-                    tabIndex={0}
-                    className="relative flex w-full rounded border-black border"
-                  >
-                    <select
-                      id="city"
-                      className="block w-full p-3 pl-5 text-sans-body1 text-black bg-transparent border-none appearance-none focus:ring-0"
-                      {...register("jobRestriction", {
-                        required: { value: true, message: "必填欄位" },
-                      })}
-                    >
-                      {occupations.map(({ id, title: occupation }) => (
-                          <option value={occupation} key={id}>
-                            {occupation}
-                          </option>
-                        )
-                      )}
-                    </select>
-                    <label
-                      htmlFor="city"
-                      className="absolute text-sans-body1 text-Neutral-50 duration-200 transform -translate-y-4 scale-75 top-[3px] z-10 origin-[0] bg-white px-2  start-3"
-                    >
-                      職業類別
-                    </label>
+              {
+                job === "hasJobRestriction" && (
+                  <div className="layout-grid">
+                    <div className="col-span-6">
+                      {
+                        [...Array(jobRestrictionAmount)].map((_, index) => (
+                          <div className={
+                            index === 0 ? "mt-3" : "mt-6"
+                          } key={index}>
+                            <div
+                              tabIndex={0}
+                              className="relative flex w-full rounded border-black border"
+                            >
+                              <select
+                                id={`occupations-${index}`}
+                                className="block w-full p-3 pl-5 text-sans-body1 text-black bg-transparent border-none appearance-none focus:ring-0"
+                              >
+                                {occupations.map(({ id, title: occupation }) => (
+                                    <option value={occupation} key={index+id}>
+                                      {occupation}
+                                    </option>
+                                  )
+                                )}
+                              </select>
+                              <label
+                                htmlFor={`occupations-${index}`}
+                                className="absolute text-sans-body1 text-Neutral-50 duration-200 transform -translate-y-4 scale-75 top-[3px] z-10 origin-[0] bg-white px-2  start-3"
+                              >
+                                職業類別
+                              </label>
+                            </div>
+                          </div>
+                        ))
+                      }
+                    </div>
+                    <div className="col-span-12">
+                      <button type="button" className="letter-button-light"
+                        onClick={() => setJobRestrictionAmount(prev => prev + 1)}
+                      >
+                        <span>新增職業</span>
+                        <span className="material-symbols-outlined">add</span>
+                      </button>
+                    </div>
                   </div>
-                </div>
-                <div className="col-span-12">
-                  <button className="letter-button-light">
-                    <span>新增職業</span>
-                    <span className="material-symbols-outlined">add</span>
-                  </button>
-                </div>
-              </div>
+                )
+              }
             </div>
           )
         }
