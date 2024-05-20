458aeb6b1ebe8ddc421e4e846b8a5bea8feda53b
Avain <sports113499@gmail.com>
2024-05-21 01:20:19 +0800

    feat: add LandlordAnchor Component base style

--- src/components/landLordManagement/index/LandlordAnchor.tsx
+++ src/components/landLordManagement/index/LandlordAnchor.tsx
@@ -1,10 +1,48 @@
 export default function LandlordAnchor() {
   return (
-    <section className="layout-grid">
-      <div className="col-span-3 bg-Landlord-60">刊登中</div>
-      <div className="col-span-3 bg-Landlord-60">已承租</div>
-      <div className="col-span-3 bg-Landlord-60">已完成</div>
-      <div className="col-span-3 bg-Landlord-60">待評價</div>
+    <section className="pb-6 border-b border-Neutral-95">
+      <ul className="layout-grid">
+        <li className="col-span-3 text-white rounded-xl p-3 bg-Alert-70">
+          <h3 className="opacity-80 text-sans-b-h6 p-[10px]">刊登中</h3>
+          <div className="flex justify-between items-end">
+            <h4 className="px-[10px] py-3 text-sans-h2">4</h4>
+            <button className="letter-button-dark">
+              <span>查看</span>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </button>
+          </div>
+        </li>
+        <li className="col-span-3 text-white rounded-xl p-3 bg-Landlord-60">
+          <h3 className="opacity-80 text-sans-b-h6 p-[10px]">已承租</h3>
+          <div className="flex justify-between items-end">
+            <h4 className="px-[10px] py-3 text-sans-h2">4</h4>
+            <button className="letter-button-dark">
+              <span>查看</span>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </button>
+          </div>
+        </li>
+        <li className="col-span-3 text-white rounded-xl p-3 bg-Tenant-50">
+          <h3 className="opacity-80 text-sans-b-h6 p-[10px]">已完成</h3>
+          <div className="flex justify-between items-end">
+            <h4 className="px-[10px] py-3 text-sans-h2">4</h4>
+            <button className="letter-button-dark">
+              <span>查看</span>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </button>
+          </div>
+        </li>
+        <li className="col-span-3 text-white rounded-xl p-3 bg-Neutral-50">
+          <h3 className="opacity-80 text-sans-b-h6 p-[10px]">待評價</h3>
+          <div className="flex justify-between items-end">
+            <h4 className="px-[10px] py-3 text-sans-h2">4</h4>
+            <button className="letter-button-dark">
+              <span>查看</span>
+              <span className="material-symbols-outlined">chevron_right</span>
+            </button>
+          </div>
+        </li>
+      </ul>
     </section>
   );
 }
