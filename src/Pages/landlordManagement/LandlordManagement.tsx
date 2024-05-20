3c3d0d0f35198ba92aa1d20ab56a5b5169defdca
Avain <sports113499@gmail.com>
2024-05-20 22:40:05 +0800

    feat: add btn link to each page

--- src/Pages/landlordManagement/LandlordManagement.tsx
+++ src/Pages/landlordManagement/LandlordManagement.tsx
@@ -1,17 +1,35 @@
-import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { useLocation, useNavigate, Link, Outlet } from "react-router-dom";
 import Footer from "../../components/footer/Footer";
 import addIcon from "../../assets/imgs/icons/add.svg";
 
 export default function LandlordManagement() {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const [activeButton, setActiveButton] = useState('');
+
+  useEffect(() => {
+    if(location.pathname === '/landlord-management') {
+      setActiveButton('all');
+    } else if (location.pathname === '/landlord-management/history') {
+      setActiveButton('history');
+    }
+  },[location]);
+
   return (
     <>
       <header className="bg-Landlord-99">
         <div className="container px-8 py-6">
           <h2 className="text-sans-b-h5 mb-5">房東好窩</h2>
           <div className="flex items-end gap-3">
-            <button className="tab-button-m py-1">全部房源</button>
-            <button className="tab-button-m py-1">出租歷史</button>
-            <button className="filled-button-m px-11 ml-auto flex justify-center items-center gap-2 ">
+            <Link to="/landlord-management" className={`tab-button-m py-1 ${activeButton === 'all' ? 'bg-Landlord-50 text-Landlord-99' : ""}`}>全部房源</Link>
+            <Link to="/landlord-management/history" className={`tab-button-m py-1 ${activeButton === 'history' ? 'bg-Landlord-50 text-Landlord-99' : ""}`}>出租歷史</Link>
+            <button
+              className="filled-button-m px-11 ml-auto flex justify-center items-center gap-2 "
+              onClick={() => {
+                navigate("/post");
+              }}
+            >
               <span>新增房源</span>
               <img src={addIcon} alt="add" />
             </button>
