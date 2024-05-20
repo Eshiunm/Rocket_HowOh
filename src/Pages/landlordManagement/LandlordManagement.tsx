677ca008e04827e5688f1a3ec4fd2ad2da45a44b
Avain <sports113499@gmail.com>
2024-05-20 22:28:40 +0800

    feat: add landlord-management page header and Footer, modify index.css btn disable style

--- src/Pages/landlordManagement/LandlordManagement.tsx
+++ src/Pages/landlordManagement/LandlordManagement.tsx
@@ -1,23 +1,25 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../../components/footer/Footer";
 import addIcon from "../../assets/imgs/icons/add.svg";
 
 export default function LandlordManagement() {
   return (
     <>
-      <div className="container">
-        <div className="mt-[52px] bg-Landlord-95 rounded-xl p-6">
-          <h2 className="text-sans-b-h5 mb-7">房東好窩</h2>
-          <div className="flex gap-3">
-            <button className="text-sans-b-body1 bg-Landlord-30 text-Landlord-99 px-4 py-2 rounded-3xl">全部房源</button>
-            <button className="text-sans-b-body1 bg-transparent border border-Landlord-40 text-black px-4 py-2 rounded-3xl">出租歷史</button>
-            <button className="text-sans-b-h6 ml-auto px-20 py-3 rounded-lg bg-black flex justify-center items-center gap-3 text-white">
-              <span className="">新增房源</span>
+      <header className="bg-Landlord-99">
+        <div className="container px-8 py-6">
+          <h2 className="text-sans-b-h5 mb-5">房東好窩</h2>
+          <div className="flex items-end gap-3">
+            <button className="tab-button-m py-1">全部房源</button>
+            <button className="tab-button-m py-1">出租歷史</button>
+            <button className="filled-button-m px-11 ml-auto flex justify-center items-center gap-2 ">
+              <span>新增房源</span>
               <img src={addIcon} alt="add" />
             </button>
           </div>
         </div>
-      </div>
+      </header>
       <Outlet />
+      <Footer />
     </>
   );
 }
