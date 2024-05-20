b63e7eb3cc75c8f8c0329d377e9f74c7efd2c347
Eshiunm <eshiunm@gmail.com>
2024-05-20 21:39:17 +0800

    refactor:fix types errors

--- src/Components/signUp/imgUpload/setCanvasPreview.ts
+++ src/Components/signUp/imgUpload/setCanvasPreview.ts
@@ -1,7 +1,9 @@
+import { PixelCrop } from "react-image-crop";
+
 const setCanvasPreview = (
   image: HTMLImageElement | null,
   canvas: HTMLCanvasElement | null,
-  crop
+  crop: PixelCrop
 ) => {
   const ctx = canvas?.getContext("2d");
   if (!ctx) {
@@ -13,9 +15,12 @@ const setCanvasPreview = (
   // size the image back down if you want to download/upload and be
   // true to the images natural size.
   const pixelRatio = window.devicePixelRatio;
-  const scaleX = image?.naturalWidth / image.width;
-  const scaleY = image.naturalHeight / image.height;
+  const scaleX = image!.naturalWidth / image!.width || 1;
+  const scaleY = image!.naturalHeight / image!.height || 1;
 
+  if (!canvas) {
+    throw new Error("Canvas is null");
+  }
   canvas.width = Math.floor(crop.width * scaleX * pixelRatio);
   canvas.height = Math.floor(crop.height * scaleY * pixelRatio);
 
@@ -28,16 +33,19 @@ const setCanvasPreview = (
 
   // Move the crop origin to the canvas origin (0,0)
   ctx.translate(-cropX, -cropY);
+  if (!image) {
+    throw new Error("Image is null");
+  }
   ctx.drawImage(
     image,
     0,
     0,
-    image.naturalWidth,
-    image.naturalHeight,
+    image?.naturalWidth,
+    image?.naturalHeight,
     0,
     0,
-    image.naturalWidth,
-    image.naturalHeight
+    image?.naturalWidth,
+    image?.naturalHeight
   );
 
   ctx.restore();
