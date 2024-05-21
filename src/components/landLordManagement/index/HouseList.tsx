eca93ebc3e2ff7abfd1bedf277d303a23d59173a
Avain <sports113499@gmail.com>
2024-05-21 17:35:16 +0800

    feat: add landlord house cards base Componenet style and add list in management page

--- src/components/landLordManagement/index/HouseList.tsx
+++ src/components/landLordManagement/index/HouseList.tsx
@@ -1,5 +1,6 @@
-import LandlordAnchor from "./LandlordAnchor";
 import { CustomFlowbiteTheme, Flowbite, Accordion, AccordionContent, AccordionPanel, AccordionTitle } from "flowbite-react";
+import LandlordAnchor from "./LandlordAnchor";
+import HouseCard from "./HouseCard";
 
 export default function HouseList() {
   const customTheme: CustomFlowbiteTheme = {
@@ -12,7 +13,7 @@ export default function HouseList() {
         }
       },
       "content": {
-        "base": "p-5 first:rounded-t-lg last:rounded-b-lg"
+        "base": "py-6 first:rounded-t-lg last:rounded-b-lg"
       },
       "title": {
         "arrow": {
@@ -48,20 +49,9 @@ export default function HouseList() {
               新增中
             </AccordionTitle>
             <AccordionContent>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Flowbite is an open-source library of interactive components built on top of Tailwind CSS including buttons,
-                dropdowns, modals, navbars, and more.
-              </p>
-              <p className="text-gray-500 dark:text-gray-400">
-                Check out this guide to learn how to&nbsp;
-                <a
-                  href="https://flowbite.com/docs/getting-started/introduction/"
-                  className="text-cyan-600 hover:underline dark:text-cyan-500"
-                >
-                  get started&nbsp;
-                </a>
-                and start developing websites even faster with components on top of Tailwind CSS.
-              </p>
+              <ul className="layout-grid">
+                <HouseCard />
+              </ul>
             </AccordionContent>
           </AccordionPanel>
           <AccordionPanel>
@@ -69,17 +59,9 @@ export default function HouseList() {
               刊登中
             </AccordionTitle>
             <AccordionContent>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                Flowbite is first conceptualized and designed using the Figma software so everything you see in the library
-                has a design equivalent in our Figma file.
-              </p>
-              <p className="text-gray-500 dark:text-gray-400">
-                Check out the
-                <a href="https://flowbite.com/figma/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                  Figma design system
-                </a>
-                based on the utility classes from Tailwind CSS and components from Flowbite.
-              </p>
+              <ul className="layout-grid">
+                <HouseCard />
+              </ul>
             </AccordionContent>
           </AccordionPanel>
           <AccordionPanel>
@@ -87,31 +69,8 @@ export default function HouseList() {
               已承租
             </AccordionTitle>
             <AccordionContent>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                The main difference is that the core components from Flowbite are open source under the MIT license, whereas
-                Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
-                components, whereas Tailwind UI offers sections of pages.
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
-                technical reason stopping you from using the best of two worlds.
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-              <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-                <li>
-                  <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                    Flowbite Pro
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://tailwindui.com/"
-                    rel="nofollow"
-                    className="text-cyan-600 hover:underline dark:text-cyan-500"
-                  >
-                    Tailwind UI
-                  </a>
-                </li>
+              <ul className="layout-grid">
+                <HouseCard />
               </ul>
             </AccordionContent>
           </AccordionPanel>
@@ -120,31 +79,8 @@ export default function HouseList() {
               已完成
             </AccordionTitle>
             <AccordionContent>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                The main difference is that the core components from Flowbite are open source under the MIT license, whereas
-                Tailwind UI is a paid product. Another difference is that Flowbite relies on smaller and standalone
-                components, whereas Tailwind UI offers sections of pages.
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">
-                However, we actually recommend using both Flowbite, Flowbite Pro, and even Tailwind UI as there is no
-                technical reason stopping you from using the best of two worlds.
-              </p>
-              <p className="mb-2 text-gray-500 dark:text-gray-400">Learn more about these technologies:</p>
-              <ul className="list-disc pl-5 text-gray-500 dark:text-gray-400">
-                <li>
-                  <a href="https://flowbite.com/pro/" className="text-cyan-600 hover:underline dark:text-cyan-500">
-                    Flowbite Pro
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="https://tailwindui.com/"
-                    rel="nofollow"
-                    className="text-cyan-600 hover:underline dark:text-cyan-500"
-                  >
-                    Tailwind UI
-                  </a>
-                </li>
+              <ul className="layout-grid">
+                <HouseCard />
               </ul>
             </AccordionContent>
           </AccordionPanel>
