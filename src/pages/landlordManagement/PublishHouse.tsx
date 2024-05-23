9add32233ba7ea22875ea269aaf3193c41a3266c
Avain <sports113499@gmail.com>
2024-05-23 14:16:00 +0800

    feat: add publish house page header

--- src/pages/landlordManagement/PublishHouse.tsx
+++ src/pages/landlordManagement/PublishHouse.tsx
@@ -1,3 +1,26 @@
 export default function PublishHouse() {
-  return <div>PublishHouse123</div>;
+  return (
+    <>
+      <header className="bg-Landlord-99">
+        <div className="container py-6">
+          <span className="badge-m  bg-Landlord-90">刊登中</span>
+          <h2 className="text-sans-b-h5 mt-3 mb-6">信義國小套房 捷運3分鐘</h2>
+          <div className="flex justify-between items-end">
+            <div>
+              <h6 className="mb-2 text-sans-body2">變更狀態</h6>
+              <div className="flex gap-3">
+                <button className="outline-button-m">更改為已承租</button>
+                <button className="outline-button-m">更改為已完成</button>
+              </div>
+            </div>
+            <div className="flex gap-6">
+              <button className="outline-button-m">返回房源管理頁面</button>
+              <button className="outline-button-m">查看合約</button>
+              <button className="filled-button-m">查看租客預約請求</button>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
 }
