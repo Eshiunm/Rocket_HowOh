bfa96a1861386e3b184f8ec729bd124fa038af78
Avain <sports113499@gmail.com>
2024-05-25 22:39:32 +0800

    feat: add delete house popup model

--- src/components/landLordManagement/posts/Post.tsx
+++ src/components/landLordManagement/posts/Post.tsx
@@ -1,7 +1,14 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import { procedureListType } from "../../../types/procedureList";
+// Model-popup 所需之匯入
+import { Modal } from "flowbite-react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 export default function Post({procedure}:{procedure:procedureListType[]}) {
+  // 控制 Model-popup 的開關
+  const [openModal, setOpenModal] = useState(true);
+
   return (
     <div className="col-span-10 col-start-2 flex justify-between">
       <h2 className="text-sans-b-h5">新增房源</h2>
@@ -11,7 +18,36 @@ export default function Post({procedure}:{procedure:procedureListType[]}) {
           className={`${procedure[0].isActive ? "outline-button-m-disable" : "outline-button-m"} px-4`}
           disabled={procedure[0].isActive ? true : false}
           // 在基本資訊頁面時不可點擊刪除房源
-          >刪除房源</button>
+          onClick={() => setOpenModal(true)}
+        >
+          刪除房源
+        </button>
+        {/* 點擊刪除房源跳出的 Model pop-up */}
+        <Modal show={openModal} size="lg" onClose={() => setOpenModal(false)} popup>
+          <Modal.Body className="p-10">
+            <div className="flex items-center gap-3 mb-10">
+              <HiOutlineExclamationCircle className="h-6 w-6 text-Alert-50" />
+              <h3 className="text-sans-h5 text-Alert-50">
+                警示
+              </h3>
+              <span
+                className="material-symbols-outlined ml-auto cursor-pointer"
+                onClick={() => setOpenModal(false)}
+              >
+                close
+              </span>
+            </div>
+            <p className="mb-10 text-sans-body1">此動作不可返回，您確定要刪除房源？</p>
+            <div className="flex justify-end gap-6">
+              <button className="outline-button-m" onClick={() => setOpenModal(false)}>
+                確認刪除
+              </button>
+              <button className="filled-button-m" onClick={() => setOpenModal(false)}>
+                返回
+              </button>
+            </div>
+          </Modal.Body>
+        </Modal>    
       </div>
     </div>
   );
