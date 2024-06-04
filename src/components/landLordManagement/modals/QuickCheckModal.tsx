f8df4a14c37d1ae125b873cb1f7245dbe1ce6870
Avain <sports113499@gmail.com>
2024-06-04 14:50:21 +0800

    feat: add quick check modal search tenant loading

--- src/components/landLordManagement/modals/QuickCheckModal.tsx
+++ src/components/landLordManagement/modals/QuickCheckModal.tsx
@@ -113,10 +113,16 @@ export default function QuickCheckModal(props : QuickCheckModalPropsType) {
               }
               {isPosting ? (
                 <div role="status" className="bg-Landlord-95 rounded-lg p-3 my-3 flex items-center gap-6">
-                  <div className="animate-pulse w-16 h-16 rounded-lg flex items-center justify-center bg-Neutral-90">
-                    <Spinner color="info" size="xl" />
+                  <div className="animate-pulse flex items-center justify-center w-16 h-16 bg-gray-300 rounded">
+                    <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                      <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                    </svg>
                   </div>
-                  <div className="animate-pulse w-3/5 h-6 bg-Neutral-90 rounded-full me-3"></div>
+                  <div className="animate-pulse w-3/5">
+                    <div className="h-2.5 bg-gray-200 rounded-full w-48 my-4"></div>
+                    <div className="h-2 bg-gray-200 rounded-full max-w-[480px] mb-2.5"></div>
+                  </div>
+                  <span className="sr-only">Loading...</span>
                 </div>
                 ) 
                 : tenantInfo ? (
