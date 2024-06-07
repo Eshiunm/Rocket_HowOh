9c21d31291071ec5b078745544e18e4abee0c014
Avain <sports113499@gmail.com>
2024-06-07 10:38:43 +0800

    feat: add tenant info loading

--- src/components/landLordManagement/request/RequestCard.tsx
+++ src/components/landLordManagement/request/RequestCard.tsx
@@ -41,17 +41,27 @@ export default function RequestCard({data, status = "none"}: {data: RequestListT
   };
 
   const [isOpen, setIsOpen] = useState(false);
+  const [getInfoLoading, setGetInfoLoading] = useState(false);
   const handleClose = () => setIsOpen(false);
 
   const { appointmentId, appointmentTime, descrption } = data;
   const { tenantInfo, orderInfo } = descrption;
 
+  const getSingleTenantInfo = (appointmentId: number) => {
+    setGetInfoLoading(true);
+    console.log(appointmentId);
+  };
+
+  const handleCardClick = () => {
+    setIsOpen(true);
+    getSingleTenantInfo(appointmentId);
+  };
+
   return (
     <>
       <li
         className={`p-3 mb-4 flex gap-4 rounded-xl hover:bg-Landlord-99 ${isOpen && "bg-Landlord-95"}`}
-        onClick={() => setIsOpen(true)}
-        data-appointmentId={appointmentId}
+        onClick={handleCardClick}
       >
         <div className="w-36 h-24 rounded-2xl overflow-hidden">
           <img
@@ -123,65 +133,111 @@ export default function RequestCard({data, status = "none"}: {data: RequestListT
               >
                 <img src={close} alt="close" />
               </button>
-              <button
-                type="button"
-                className="outline-button-m flex items-center gap-2"
-              >
-                隱藏租客
-                <span className="material-symbols-outlined text-base">visibility_off</span>
-              </button>
-              <div className="w-full p-6 rounded-2xl bg-white">
-                <div className="flex justify-between mb-3.5">
-                  <div>
-                    <span className="material-symbols-outlined text-Tenant-50 mb-6">face</span>
-                    <h4 className="text-sans-b-body1 text-Tenant-50 mb-3">租客</h4>
-                    <h3 className="text-sans-b-h4 mb-3">詹小美</h3>
-                    <div className="text-sans-body1 flex mb-2">
-                      <h4 className="pr-2 mr-2 border-r border-Tenant-70">女</h4>
-                      <h4>文教類</h4>
+              {
+                getInfoLoading ? (
+                  <>
+                    <div role="status" className="w-full p-6 rounded-2xl bg-white border border-gray-200 shadow animate-pulse md:p-6">
+                      <div className="flex justify-between mb-3.5">
+                        <div>
+                          <span className="material-symbols-outlined text-Tenant-50 mb-6">face</span>
+                          <div className="h-4 bg-Neutral-95 rounded-full w-8 mb-3" />
+                          <div className="h-7 bg-Neutral-95 rounded-full w-24 mb-3" />
+                          <div className="h-4 bg-Neutral-99 rounded-full w-20 mb-2" />
+                          <div className="h-4 bg-Neutral-99 rounded-full w-28" />
+                        </div>
+                        <div className="w-[186px] rounded-lg flex items-center justify-center h-40 mb-4 bg-Neutral-99">
+                          <svg className="w-10 h-10 text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                            <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"/>
+                            <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"/>
+                          </svg>
+                        </div>
+                      </div>
+                      <div className="flex gap-3 mb-6">
+                        <div className="flex-1 p-4 rounded-lg bg-Neutral-95 flex gap-4 flex-wrap justify-between">
+                          <h5 className="w-full text-Tenant-50 text-sans-b-body1">評價</h5>
+                          <h4 className="text-sans-h4">0</h4>
+                          <img src={star} alt="star" className="self-end" />
+                        </div>
+                        <div className="flex-1 p-4 rounded-lg bg-Neutral-95 flex gap-4 flex-wrap justify-between">
+                          <h5 className="w-full text-Tenant-50 text-sans-b-body1">則數</h5>
+                          <h4 className="text-sans-h4">0</h4>
+                          <h6 className="self-end">則</h6>
+                        </div>
+                      </div>
+                      <div>
+                        <h4 className="text-Landlord-40 text-sans-b-body1 mb-3">自我介紹</h4>
+                        <div className="h-4 bg-Neutral-99 rounded-full max-w-60 mb-2" />
+                        <div className="h-4 bg-Neutral-95 rounded-full w-full mb-2" />
+                        <div className="h-4 bg-Neutral-99 rounded-full max-w-72 mb-2" />
+                        <div className="h-4 bg-Neutral-95 rounded-full max-w-24" />
+                      </div>
+                      <span className="sr-only">Loading...</span>
                     </div>
-                    <a href="tel:+886-958-230-1239" className="text-sans-body1">0958-230-1239</a>
-                  </div>
-                  <div className="w-[186px] rounded-lg overflow-hidden">
-                    <img src={photo} alt="tenant" className="w-full aspect-square block object-cover" />
-                  </div>
-                </div>
-                <div className="flex gap-3 mb-6">
-                  <div className="flex-1 p-4 rounded-lg bg-Neutral-95 flex gap-4 flex-wrap justify-between">
-                    <h5 className="w-full text-Tenant-50 text-sans-b-body1">評價</h5>
-                    <h4 className="text-sans-h4">4.8</h4>
-                    <img src={star} alt="star" className="self-end" />
-                  </div>
-                  <div className="flex-1 p-4 rounded-lg bg-Neutral-95 flex gap-4 flex-wrap justify-between">
-                    <h5 className="w-full text-Tenant-50 text-sans-b-body1">則數</h5>
-                    <h4 className="text-sans-h4">3</h4>
-                    <h6 className="self-end">則</h6>
-                  </div>
-                </div>
-                <div>
-                  <h4 className="text-Landlord-40 text-sans-b-body1 mb-3">自我介紹</h4>
-                  <p className="text-sans-body1">我是來自台中的補習班老師。最近決定轉職成為前端工程師，於是報名參加了高雄六角的培訓營！我希望能在開課前找到便宜、便利，而且能申請租屋補助的房源，同時也希望能遇到一位優質的房東。</p>
-                </div>
-              </div>
-              <div className="w-full">
-                <h4 className="text-sans-b-h6 mb-4">房東評價</h4>
-                <div className="bg-white rounded-lg p-4">
-                  <div className="flex flex-col gap-2">
-                    <h5 className="text-sans-b-body1 text-Landlord-50">王太太</h5>
-                    <time className="text-sans-body1">2023年4月22日</time>
-                    <div className="flex gap-2">
-                      <img src={star} alt="star" />
-                      <img src={star} alt="star" />
-                      <img src={star} alt="star" />
+                  </>
+                ):(
+                  <>
+                    <button
+                      type="button"
+                      className="outline-button-m flex items-center gap-2"
+                    >
+                      隱藏租客
+                      <span className="material-symbols-outlined text-base">visibility_off</span>
+                    </button>
+                    <div className="w-full p-6 rounded-2xl bg-white">
+                      <div className="flex justify-between mb-3.5">
+                        <div>
+                          <span className="material-symbols-outlined text-Tenant-50 mb-6">face</span>
+                          <h4 className="text-sans-b-body1 text-Tenant-50 mb-3">租客</h4>
+                          <h3 className="text-sans-b-h4 mb-3">詹小美</h3>
+                          <div className="text-sans-body1 flex mb-2">
+                            <h4 className="pr-2 mr-2 border-r border-Tenant-70">女</h4>
+                            <h4>文教類</h4>
+                          </div>
+                          <a href="tel:+886-958-230-1239" className="text-sans-body1">0958-230-1239</a>
+                        </div>
+                        <div className="w-[186px] rounded-lg overflow-hidden">
+                          <img src={photo} alt="tenant" className="w-full aspect-square block object-cover" />
+                        </div>
+                      </div>
+                      <div className="flex gap-3 mb-6">
+                        <div className="flex-1 p-4 rounded-lg bg-Neutral-95 flex gap-4 flex-wrap justify-between">
+                          <h5 className="w-full text-Tenant-50 text-sans-b-body1">評價</h5>
+                          <h4 className="text-sans-h4">4.8</h4>
+                          <img src={star} alt="star" className="self-end" />
+                        </div>
+                        <div className="flex-1 p-4 rounded-lg bg-Neutral-95 flex gap-4 flex-wrap justify-between">
+                          <h5 className="w-full text-Tenant-50 text-sans-b-body1">則數</h5>
+                          <h4 className="text-sans-h4">3</h4>
+                          <h6 className="self-end">則</h6>
+                        </div>
+                      </div>
+                      <div>
+                        <h4 className="text-Landlord-40 text-sans-b-body1 mb-3">自我介紹</h4>
+                        <p className="text-sans-body1">我是來自台中的補習班老師。最近決定轉職成為前端工程師，於是報名參加了高雄六角的培訓營！我希望能在開課前找到便宜、便利，而且能申請租屋補助的房源，同時也希望能遇到一位優質的房東。</p>
+                      </div>
                     </div>
-                    <p className="mt-2 text-sans-body1">黃同學乾淨有禮貌，好租客！</p>
-                  </div>
-                  <div className="mt-4 pt-6 border-t border-Neutral-95">
-                    <h5 className="text-sans-b-body1 text-Landlord-50 mb-4">租客回覆</h5>
-                    <p className="mt-2 text-sans-body1">謝謝王媽媽照顧！</p>
-                  </div>
-                </div>
-              </div>
+                    <div className="w-full">
+                      <h4 className="text-sans-b-h6 mb-4">房東評價</h4>
+                      <div className="bg-white rounded-lg p-4">
+                        <div className="flex flex-col gap-2">
+                          <h5 className="text-sans-b-body1 text-Landlord-50">王太太</h5>
+                          <time className="text-sans-body1">2023年4月22日</time>
+                          <div className="flex gap-2">
+                            <img src={star} alt="star" />
+                            <img src={star} alt="star" />
+                            <img src={star} alt="star" />
+                          </div>
+                          <p className="mt-2 text-sans-body1">黃同學乾淨有禮貌，好租客！</p>
+                        </div>
+                        <div className="mt-4 pt-6 border-t border-Neutral-95">
+                          <h5 className="text-sans-b-body1 text-Landlord-50 mb-4">租客回覆</h5>
+                          <p className="mt-2 text-sans-body1">謝謝王媽媽照顧！</p>
+                        </div>
+                      </div>
+                    </div>
+                  </>
+                )
+              }
             </div>
           </div>
         </Drawer.Items>
