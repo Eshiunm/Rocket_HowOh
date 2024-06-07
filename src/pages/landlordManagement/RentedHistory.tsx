0e71077983bc1b0d946fd9cf31c0b9f116bbf649
Avain <sports113499@gmail.com>
2024-06-07 20:18:33 +0800

    feat: add list loading skeleton

--- src/pages/landlordManagement/RentedHistory.tsx
+++ src/pages/landlordManagement/RentedHistory.tsx
@@ -17,15 +17,18 @@ export type ExpiredListType = {
 
 export default function RentedHistory() {
   const [expiredList, setExpiredList] = useState<ExpiredListType[]>([]);
+  const [getListLoading, setGetListLoading] = useState<boolean>(false);
 
   useEffect(() => {
     const getExpiredList = async () => {
+      setGetListLoading(true);
       try {
         const response = await apiOrderLandlordListExpired();
         setExpiredList(response.data.data);
       } catch (error) {
         console.log(error);
       }
+      setGetListLoading(false);
     }
     getExpiredList();
   },[])
@@ -35,6 +38,27 @@ export default function RentedHistory() {
       <div className="col-span-7 p-5">
         <section>
           <ul>
+            {
+              getListLoading && 
+              [...Array(5)].map((_, index) => (
+                <li key={`"requestListLoading"${index}`}>
+                  <div role="status" className="p-3 space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center">
+                    <div className="flex items-center justify-center w-32 h-32 bg-gray-300 rounded-2xl overflow-hidden">
+                      <svg className="w-10 h-10 text-gray-200" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                        <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z"/>
+                      </svg>
+                    </div>
+                    <div className="w-auto flex-1">
+                      <div className="h-4 bg-gray-200 rounded-full w-48 mb-5"></div>
+                      <div className="h-2 bg-gray-200 rounded-full mb-3"></div>
+                      <div className="h-2 bg-gray-200 rounded-full max-w-[440px] mb-3"></div>
+                      <div className="h-2 bg-gray-200 rounded-full max-w-[360px]"></div>
+                    </div>
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                </li>
+              ))
+            }
             {
               expiredList.map((list, index) => <HistoryCard key={`expiredlist+${index}`} data={list} />)
             }
