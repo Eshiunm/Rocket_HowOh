284a11174b9c8a798fb6cada8e1f0c712123907b
Eshiunm <eshiunm@gmail.com>
2024-06-12 01:31:51 +0800

    fix: change class= to className=

--- src/components/singleHousePage/SingleHousePageSkeleton.tsx
+++ src/components/singleHousePage/SingleHousePageSkeleton.tsx
@@ -6,9 +6,9 @@ function SingleHousePageSkeleton() {
         <div className="container layout-grid pt-6 mb-6">
           {/* 首圖 */}
           <div className="col-span-6 col-start-2 overflow-hidden">
-            <div class="animate-pulse flex items-center justify-center w-full h-full bg-gray-300 rounded ">
+            <div className="animate-pulse flex items-center justify-center w-full h-full bg-gray-300 rounded ">
               <svg
-                class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                className="w-10 h-10 text-gray-200 dark:text-gray-600"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -20,9 +20,9 @@ function SingleHousePageSkeleton() {
           </div>
           {/* 次要圖片 */}
           <div className="col-span-4 flex flex-wrap gap-6">
-            <div class="animate-pulse flex items-center justify-center w-[200px] h-[200px] bg-gray-300 rounded ">
+            <div className="animate-pulse flex items-center justify-center w-[200px] h-[200px] bg-gray-300 rounded ">
               <svg
-                class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                className="w-10 h-10 text-gray-200 dark:text-gray-600"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -31,9 +31,9 @@ function SingleHousePageSkeleton() {
                 <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
               </svg>
             </div>
-            <div class="animate-pulse flex items-center justify-center w-[200px] h-[200px] bg-gray-300 rounded ">
+            <div className="animate-pulse flex items-center justify-center w-[200px] h-[200px] bg-gray-300 rounded ">
               <svg
-                class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                className="w-10 h-10 text-gray-200 dark:text-gray-600"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -42,9 +42,9 @@ function SingleHousePageSkeleton() {
                 <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
               </svg>
             </div>
-            <div class="animate-pulse flex items-center justify-center w-[200px] h-[200px] bg-gray-300 rounded ">
+            <div className="animate-pulse flex items-center justify-center w-[200px] h-[200px] bg-gray-300 rounded ">
               <svg
-                class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                className="w-10 h-10 text-gray-200 dark:text-gray-600"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -53,9 +53,9 @@ function SingleHousePageSkeleton() {
                 <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
               </svg>
             </div>
-            <div class="animate-pulse flex items-center justify-center w-[200px] h-[200px] bg-gray-300 rounded ">
+            <div className="animate-pulse flex items-center justify-center w-[200px] h-[200px] bg-gray-300 rounded ">
               <svg
-                class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                className="w-10 h-10 text-gray-200 dark:text-gray-600"
                 aria-hidden="true"
                 xmlns="http://www.w3.org/2000/svg"
                 fill="currentColor"
@@ -72,7 +72,7 @@ function SingleHousePageSkeleton() {
           {/* 房源描述 */}
           <div className="col-span-6 col-start-2">
             {/* 房源標題 */}
-            <div class="h-10 bg-gray-200 rounded-full w-[360px] mb-6"></div>
+            <div className="h-10 bg-gray-200 rounded-full w-[360px] mb-6"></div>
             {/* 房源特色 */}
             <ul className="flex gap-x-4 mb-10">
               <li className="h-10 bg-gray-200 rounded-full w-[176px]"></li>
@@ -312,16 +312,16 @@ function SingleHousePageSkeleton() {
           <div className="col-span-3 col-start-9 ">
             {/* 查看更多照片 */}
             <div className="flex justify-between">
-              <div class="w-40 h-9 ml-auto bg-gray-300 rounded-xl mb-4"></div>
+              <div className="w-40 h-9 ml-auto bg-gray-300 rounded-xl mb-4"></div>
             </div>
 
             <div className="sticky top-[80px]">
               {/* 預約看房 */}
               <div className=" shadow-elevation-3 rounded-2xl p-6 mb-[14px]">
                 <ul className="flex flex-col items-center gap-y-[34px] ">
-                  <li class="w-40 h-9 bg-gray-300 rounded-xl"></li>
-                  <li class="w-20 h-3 bg-gray-300 rounded-xl"></li>
-                  <li class="w-full h-9 bg-gray-300 rounded-xl"></li>
+                  <li className="w-40 h-9 bg-gray-300 rounded-xl"></li>
+                  <li className="w-20 h-3 bg-gray-300 rounded-xl"></li>
+                  <li className="w-full h-9 bg-gray-300 rounded-xl"></li>
                 </ul>
               </div>
               {/* 房東基本資訊 */}
@@ -329,13 +329,13 @@ function SingleHousePageSkeleton() {
                 <ul className="flex flex-col gap-y-6">
                   <li className="flex justify-between">
                     <div className="flex flex-col justify-between">
-                      <div class="w-5 h-5 bg-gray-300 rounded-xl mb-4"></div>
-                      <div class="w-10 h-2 bg-gray-300 rounded-xl mb-4"></div>
-                      <div class="w-16 h-3 bg-gray-300 rounded-xl mb-4"></div>
+                      <div className="w-5 h-5 bg-gray-300 rounded-xl mb-4"></div>
+                      <div className="w-10 h-2 bg-gray-300 rounded-xl mb-4"></div>
+                      <div className="w-16 h-3 bg-gray-300 rounded-xl mb-4"></div>
                     </div>
-                    <div class="animate-pulse flex items-center justify-center w-[116px] h-[116px] bg-gray-300 rounded-2xl ">
+                    <div className="animate-pulse flex items-center justify-center w-[116px] h-[116px] bg-gray-300 rounded-2xl ">
                       <svg
-                        class="w-10 h-10 text-gray-200 dark:text-gray-600"
+                        className="w-10 h-10 text-gray-200 dark:text-gray-600"
                         aria-hidden="true"
                         xmlns="http://www.w3.org/2000/svg"
                         fill="currentColor"
@@ -346,8 +346,8 @@ function SingleHousePageSkeleton() {
                     </div>
                   </li>
                   <li className="flex justify-between gap-x-3">
-                    <div class="w-full h-32 bg-gray-200 rounded-xl p-6"></div>
-                    <div class="w-full h-32 bg-gray-200 rounded-xl p-6"></div>
+                    <div className="w-full h-32 bg-gray-200 rounded-xl p-6"></div>
+                    <div className="w-full h-32 bg-gray-200 rounded-xl p-6"></div>
                   </li>
                 </ul>
               </div>
