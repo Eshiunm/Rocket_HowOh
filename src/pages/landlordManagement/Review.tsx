d796d2ecd3b8cb9eb6770ac93e8d8a05276d3d27
Avain <sports113499@gmail.com>
2024-06-12 14:31:30 +0800

    feat: add review page refresh web

--- src/pages/landlordManagement/Review.tsx
+++ src/pages/landlordManagement/Review.tsx
@@ -1,9 +1,24 @@
+import Footer from "../../components/footer/Footer";
+
 export default function Review() {
   return (
     <>
       <header className="bg-Landlord-99 py-6">
         <div className="container text-sans-b-h5">評價管理</div>
       </header>
+      <main className="container layout-grid mt-6">
+        <div className="col-span-7 px-5">
+          <nav className="py-3.5 mb-4 border-b border-Neutral-95">
+            <button className="outline-button-s mb-3">
+              重新整理
+            </button>
+          </nav>
+          <section>
+            評價列表
+          </section>
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
