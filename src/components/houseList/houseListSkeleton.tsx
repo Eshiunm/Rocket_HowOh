0050d8d798a9f1610e7e05b1665f72cfe1f6e948
Eshiunm <eshiunm@gmail.com>
2024-06-13 15:40:41 +0800

    refactor: modify skeleton img background color

--- src/components/houseList/houseListSkeleton.tsx
+++ src/components/houseList/houseListSkeleton.tsx
@@ -9,13 +9,15 @@ function HouseListSkeleton() {
               role="status"
               className="p-3 space-y-8 animate-pulse md:space-y-0 md:space-x-8 rtl:space-x-reverse md:flex md:items-center"
             >
-              <div className="flex items-center justify-center  bg-gray-300 rounded-2xl ">
+              <div className="flex items-center justify-center w-[350px] h-[190px] mb-4 bg-gray-300 rounded-2xl dark:bg-gray-700">
                 <svg
-                  className="h-[189px] text-gray-200 dark:text-gray-600 "
+                  className="w-10 h-10 text-gray-200 dark:text-gray-600"
                   aria-hidden="true"
-                  viewBox="0 0 20 18"
+                  fill="currentColor"
+                  viewBox="0 0 16 20"
                 >
-                  <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+                  <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z"></path>
+                  <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z"></path>
                 </svg>
               </div>
               <div className="w-full">
