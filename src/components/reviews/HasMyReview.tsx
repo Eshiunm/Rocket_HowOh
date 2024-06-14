c0b2bce4dd346d3fd68fe2ab576f635b12f72521
Avain <sports113499@gmail.com>
2024-06-14 10:45:28 +0800

    feat: add landlord review section of comment before

--- src/components/reviews/HasMyReview.tsx
+++ src/components/reviews/HasMyReview.tsx
@@ -13,27 +13,26 @@ export default function HasMyReview ({role}:{role: string}) {
         <h5 className="text-sans-b-h6">評分</h5>
         <ShowRatingStar rate={showRating} />
       </div>
-      {/* <form
-        className="pt-6 flex flex-col"  
-        onSubmit={handleSubmit(onSubmit)}
+      <div
+        className="pt-6 flex flex-col"
       >
         <h5 className="text-sans-b-h6 mb-4">評論</h5>
-        <textarea
-          className="resize-none w-full rounded p-3 text-sans-body1 bg-transparent border-black focus:ring-0 focus:border-2 focus:border-Brand-30 focus:-m-px"
-          placeholder="請說明您的住宿經驗"
-          rows={11}
-          {...register("comment", {maxLength: 200})}
-        />
-        <small className="px-3 pt-1 text-sans-caption">
-          最多200字
-        </small>
-        <button
-          type="submit"
-          className="outline-button-m mt-2.5 self-end"
+        <p
+          className="w-full p-3 text-sans-body1 bg-transparent border-b border-black"
         >
-          送出
+          評論文字區域
+        </p>
+        <time className="px-3 pt-1 text-sans-caption text-Neutral-70">
+          2024年5月18日 14:40
+        </time>
+        <button
+          type="button"
+          className="outline-button-m mt-2.5 self-end"
+          disabled
+        >
+          評價已送出
         </button>
-      </form> */}
+      </div>
     </section>
   )
 }
