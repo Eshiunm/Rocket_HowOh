083ab28d8961602bea998019c6d0ecbad07252e6
Eshiunm <eshiunm@gmail.com>
2024-06-18 21:16:54 +0800

    feat: completed layout

--- src/components/buttons/RefreshBtn.tsx
+++ src/components/buttons/RefreshBtn.tsx
@@ -1,8 +1,14 @@
-export default function RefreshBtn () {
+export default function RefreshBtn() {
   return (
-    <button className="outline-button-s mb-3 flex gap-1 items-center">
+    <button
+      type="button"
+      className="outline-button-s mb-3 flex gap-1 items-center"
+      onClick={() => window.location.reload()}
+    >
       重新整理
-      <span className="material-symbols-outlined text-base leading-4">autorenew</span>
+      <span className="material-symbols-outlined text-base leading-4">
+        autorenew
+      </span>
     </button>
   );
 }
--- src/pages/tenantManagement/FeedbackManagementPage.tsx
+++ src/pages/tenantManagement/FeedbackManagementPage.tsx
@@ -6,7 +6,7 @@ function FeedbackManagementPage() {
     <>
       <section className="py-6 mb-6 border-b border-t border-Neutral-95 bg-Neutral-99">
         <div className="container">
-          <h2 className="text-sans-b-h5 ">租屋管理</h2>
+          <h2 className="text-sans-b-h5 ">評價管理</h2>
         </div>
       </section>
       <Outlet />
